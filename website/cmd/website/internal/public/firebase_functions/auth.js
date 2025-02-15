01c151635da95efeab668f0e9b696e2f89d4eb23
Shirshajit Sen Gupta <shirshajit@gmail.com>
2025-02-15 20:13:53 +0800

    Buggy push

--- website/cmd/website/internal/public/firebase_functions/auth.js
+++ website/cmd/website/internal/public/firebase_functions/auth.js
@@ -1,9 +1,25 @@
+// import { initializeApp } from "firebase/app";
+
+function initializeFirebaseApp() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCYLksm53UhnYCxI77-hNoZl_apDlWoz7k",
+      authDomain: "birds-of-a-feather-f7414.firebaseapp.com",
+      projectId: "birds-of-a-feather-f7414",
+      storageBucket: "birds-of-a-feather-f7414.firebasestorage.app",
+      messagingSenderId: "514624258311",
+      appId: "1:514624258311:web:18d517b54cf1eec93805a2"
+    };
+  
+    firebase.initializeApp(firebaseConfig);
+  }
+  
+
 const app = initializeFirebaseApp();
-const auth = getAuth(app);
-const provider = new firebase.auth.GoogleAuthProvider();
+const auth = firebase.getAuth();
+// const provider = new firebase.auth.GoogleAuthProvider();
 
 
-export const signUp = async (email, password) => {
+async function signUp(email, password) {
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
@@ -12,8 +28,7 @@ export const signUp = async (email, password) => {
         console.error(error);
     }
 }
-
-export const signIn = async (email, password) => {
+async function signIn(email, password) {
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
         const user = userCredential.user;
@@ -23,7 +38,7 @@ export const signIn = async (email, password) => {
     }
 }
 
-export const signInWithGoogle = async () => {
+async function signInWithGoogle() {
     try {
         const userCredential = await signInWithPopup(auth, provider);
         const user = userCredential.user;
@@ -33,11 +48,10 @@ export const signInWithGoogle = async () => {
     }
 }
 
-export const signOut = async () => {
+async function signOut() {
     try {
         await signOut(auth);
     } catch (error) {
         console.error(error);
     }
 }
-
--- website/cmd/website/internal/public/firebase_functions/user_fn.js
+++ website/cmd/website/internal/public/firebase_functions/user_fn.js
@@ -1,5 +1,19 @@
-import { collection, getFirestore } from "firebase/firestore";
+// import { collection, getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
 
+function initializeFirebaseApp() {
+    const firebaseConfig = {
+      apiKey: "AIzaSyCYLksm53UhnYCxI77-hNoZl_apDlWoz7k",
+      authDomain: "birds-of-a-feather-f7414.firebaseapp.com",
+      projectId: "birds-of-a-feather-f7414",
+      storageBucket: "birds-of-a-feather-f7414.firebasestorage.app",
+      messagingSenderId: "514624258311",
+      appId: "1:514624258311:web:18d517b54cf1eec93805a2"
+    };
+  
+    // initializeApp(firebaseConfig);
+  }
+  
 const db = getFirestore();
 
 export const createUser = async (user) => {

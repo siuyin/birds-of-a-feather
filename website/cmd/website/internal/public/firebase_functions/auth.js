46adf0160513332322c5ea65b1dc11d84879b855
Shirshajit Sen Gupta <shirshajit@gmail.com>
2025-02-15 21:54:06 +0800

    Fix auth

--- website/cmd/website/internal/public/firebase_functions/auth.js
+++ website/cmd/website/internal/public/firebase_functions/auth.js
@@ -1,4 +1,40 @@
-// import { initializeApp } from "firebase/app";
+const Type = Object.freeze({
+    EDUCATION: 'education',
+    COMPANY: 'company'
+});
+
+const Level = Object.freeze({
+    STUDENT: 'student',
+    TEACHER: 'teacher',
+    ADMIN: 'admin',
+    EVENTS: 'events',
+    LOGISTICS: 'logistics'
+});
+
+class User {
+    constructor(id, name, email, type, level) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.type = type;
+        this.level = level;
+    }
+
+    toJSON() {
+        return {
+            id: this.id,
+            name: this.name,
+            email: this.email,
+            type: this.type,
+            level: this.level
+        };
+    }
+
+    static fromJSON(json) {
+        return new User(json.id, json.name, json.email, json.type, json.level);
+    }
+
+}
 
 function initializeFirebaseApp() {
     const firebaseConfig = {
@@ -17,12 +53,41 @@ function initializeFirebaseApp() {
 const app = initializeFirebaseApp();
 // const auth = auth;
 const provider = new firebase.auth.GoogleAuthProvider();
+const db = firebase.firestore();
 
+
+async function createUser(name, email, type, level) {
+    try {
+        var userObj = {
+            name: name,
+            email: email,
+            type: type,
+            level: level
+        };
+
+        const docRef = await db.collection("users").add(userObj);
+        console.log("Document written with ID: ", docRef.id);
+        return docRef;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function readUser (userId) {
+    try {
+        const docRef = doc(db, "users", userId);
+        const docSnap = await db.collection("users").doc(userId);
+        return docSnap.data();
+    } catch (error) {
+        console.error(error);
+    }
+}
 
 async function signUp() {
     try {
         var email = document.getElementById("username").value;
         var password = document.getElementById("password").value;
+        var name = document.getElementById("name").value;
         if (email.length < 4) {
             alert('Please enter an email address.');
             return;
@@ -31,12 +96,20 @@ async function signUp() {
             alert('Please enter a password.');
             return;
         }
+        if (name.length < 4) {
+            alert('Please enter a name.');
+            return;
+        }
+        
         const userCredential = await firebase.auth().createUserWithEmailAndPassword(email, password);
         const user = userCredential.user;
         console.log(user);
         console.log("Signed Up");
+        createUser(name, email, Type.EDUCATION, Level.STUDENT);
+
         return user;
     } catch (error) {
+        alert("Error signing up" + error);
         console.error(error);
     }
 }
@@ -44,14 +117,15 @@ async function signIn() {
     try {
         var email = document.getElementById("username").value;
         var password = document.getElementById("password").value;
-        var name = document.getElementById("name").value;
-        const userCredential = firebase.auth().signInWithEmailAndPassword(email, password);
+        const userCredential = await firebase.auth().signInWithEmailAndPassword(email, password);
 
         const user = userCredential.user;
         console.log(user);
         console.log("Signed In");
+        window.location.href = "index.html";
         return user;
     } catch (error) {
+        alert("Invalid email or password");
         console.error(error);
     }
 }
@@ -60,8 +134,10 @@ async function signInWithGoogle() {
     try {
         const userCredential = await firebase.auth().signInWithPopup(provider);
         const user = userCredential.user;
+         window.location.href = "index.html";
         return user;
     } catch (error) {
+        alert("Error signing in with Google");
         console.error(error);
     }
 }
@@ -70,6 +146,7 @@ async function signOut() {
     try {
         await firebase.auth().signOut();
     } catch (error) {
+        alert("Error signing out");
         console.error(error);
     }
 }
